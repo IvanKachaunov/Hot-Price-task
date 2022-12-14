f34844ce3256974345226ebafc1831d7dea58a58
Ivan Kachaunov <ivan.kachaunov1@gmail.com>
2022-12-14 14:47:13 +0200

    added code to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let prices = document.querySelectorAll(".price");
+  prices.forEach(price => {
+    price.classList.add("hot")
+    price.innerHTML += ("🔥");
+  });
 });
